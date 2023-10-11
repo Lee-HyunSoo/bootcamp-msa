89a6fb5f7c8f0cabd3581994f7a658520a17c5a8
Lee-HyunSoo <cris_lx@naver.com>
2023-10-11 17:34:38 +0900

    stock-service

--- stock-service/src/main/java/com/lhs/stockservice/domain/entity/StockPk.java
+++ stock-service/src/main/java/com/lhs/stockservice/domain/entity/StockPk.java
@@ -6,10 +6,4 @@ import java.sql.Date;
 public class StockPk implements Serializable {
     private String stockCode;
     private Date date;
-
-
-    public static void main(String[] args) {
-        String s = "aBad";
-        s.strip()
-    }
 }
